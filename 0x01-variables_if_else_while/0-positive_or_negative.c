7dbc71d130410946fcd4cfb135cf9b6d9dd580e2
Alaa <alaa@example.com>
2023-09-08 13:15:10 -0700

    tasks

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,6 @@
 #include <stdio.h>
 /**
  * main - main block
- * Description: Get a random number and print the number
- * and if it is positive, negative, or zero
  * Return: 0
  */
 int main(void)
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 /**
  * main - main block
- * Description: Get a random number and check its last digit, compare it with 5
+ 
  * Return: 0
  */
 int main(void)
 {
 	char c = 'a';
 
-	while (c <= 'z')
+	for (;c <= 'z';c++)
 	{
 		putchar(c);
-		c++;
+	
 	}
 
 	putchar('\n');

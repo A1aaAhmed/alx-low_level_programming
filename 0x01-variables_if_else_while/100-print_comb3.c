4933562375ad170f70f985e608296a28b1e52f75
Alaa <alaa@example.com>
2023-09-08 14:12:34 -0700

    task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,30 +5,18 @@
  * Return: 0
  */
 int main(void)
-{
-	int c;
-	int d = 0;
-
-	while (d < 10)
-	{
-		c = 0;
-		while (c < 10)
-		{
-			if (d != c && d < c)
-			{
-				putchar('0' + d);
-				putchar('0' + c);
-
-				if (c + d != 17)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+{	char i = '0';
+	char j = '1';
+	for (; i <= '8'; i++){
+		for (; j <='9'; j++){
+			if (i !=  j){
+				putchar(i);
+				putchar(j);}
+			if (i != '8' && j != '9'){
+				putchar(" ");
+				putchar(",");
 			}
-
-			c++;
 		}
-		d++;
 	}
 	putchar('\n');
 	return (0);

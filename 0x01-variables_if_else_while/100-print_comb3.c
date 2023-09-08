989d26667d41b74b605301cf854ada988b0f854c
Alaa <alaa@example.com>
2023-09-08 14:35:04 -0700

    task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,6 +8,7 @@ int main(void)
 {	int i = '0';
 	int j = '1';
 	int k = '1';
+
 	for (; i <= '8'; i++)
 	{
 		for (; j <= '9'; j++)
@@ -17,7 +18,7 @@ int main(void)
 				putchar(i);
 				putchar(j);
 			}
-			if (i != '8' && j != '9')
+			if (!(i == '8' && j != '9'))
 			{
 				putchar(' ');
 				putchar(',');

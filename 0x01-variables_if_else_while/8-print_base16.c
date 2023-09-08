dc76b96b6b2368acad9c4bd6d4e431f239aa2ea0
Alaa <alaa@example.com>
2023-09-08 13:54:04 -0700

    task

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,9 +2,7 @@
 #include <stdlib.h>
 /**
  * main - Entry point
- * Description: prints all single digit numbers of base 10
- * starting from 0, followed by a new line.
- * Return: Always 0 (Success)
+ * Return: 0
  */
 int main(void)
 {

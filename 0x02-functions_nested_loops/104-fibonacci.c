12b4330450832de67416259977c04db11445f21d
Alaa <alaa@example.com>
2023-09-12 14:27:16 -0700

    tasks

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,18 +9,18 @@ int main(void)
         unsigned long int i, bef1, bef2, aft1, aft2, bef = 1, aft = 2;
         unsigned long int l = 1000000000;
 
-        printf("%lu", bef);
+	printf("%lu", bef);
 
-        for (i = 1; i < 91; i++)
-        {
-                printf(", %lu", aft);
-                aft += bef;
-                bef = aft - bef;
-        }
+	for (i = 1; i < 91; i++)
+	{
+		printf(", %lu", aft);
+		aft += bef;
+		bef = aft - bef;
+	}
 
-        bef1 = (bef / l);
-        bef2 = (bef % l);
-        aft1 = (aft / l); 
+	bef1 = (bef / l);
+	bef2 = (bef % l);
+	aft1 = (aft / l);
 	aft2 = (aft % l);
 
 	for (i = 92; i < 99; ++i)

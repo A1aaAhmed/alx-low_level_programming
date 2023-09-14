4dbd20e6a1a12ae1cf048234c5d7d3593e59134a
Alaa <alaa@example.com>
2023-09-14 15:04:10 -0700

    tasks

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,20 +9,22 @@ void print_most_numbers(void)
 {
 	char c;
 	char x;
-
-	for (c = '0'; c <= '9' + 5; c++)
-	{
-
-			if (c > '9')
-			{
-				_putchar('1');
-				x = c - 10;
-			}
-			else
-				x = c;
-			_putchar(x);
-
-	}
-	_putchar('\n');
+    for (i = 0; i < 10; i++)
+    {
+    	for (c = '0'; c <= '9' + 5; c++)
+    	{
+    
+    			if (c > '9')
+    			{
+    				_putchar('1');
+    				x = c - 10;
+    			}
+    			else
+    				x = c;
+    			_putchar(x);
+    
+    	}
+    	_putchar('\n');
+    }
 
 }
